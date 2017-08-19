dce2660a47ba0f59b3ed5f83c0f921e574458b67
simon-norman <01secn@gmail.com>
2017-08-19 19:33:23 +0100

    First commit

---
+++ walkthrough-v4/Assets/Scripts/CharacterAnimate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimate : MonoBehaviour {
+
+    static Animator anim;
+    public PhotonView graphicsPV;
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+
+        anim = GetComponent<Animator>();
+        anim.SetBool("IsRunning", false);
+        anim.SetBool("IsStandard_idle", true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rb.velocity.magnitude > 0.5)
+        {
+            anim.SetBool("IsRunning", true);
+            anim.SetBool("IsStandard_idle", false);
+            // graphicsPV.RPC("playAnimPV", PhotonTargets.All, "running");
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+            anim.SetBool("IsStandard_idle", true);
+            //graphicsPV.RPC("playAnimPV", PhotonTargets.All, "standard_idle");
+        }
+
+    }
+
+}
---
+++ walkthrough-v4/Assets/Scripts/WalkthroughGameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+    public class WalkthroughGameManager : Photon.MonoBehaviour
+    {
+        public string verNum = "0.1";
+        public string roomName = "room01";
+        public Transform spawnPoint;
+        public GameObject playerPref;
+        public bool isConnected = false;
+
+
+        void Start()
+        {
+            Debug.Log("Start");
+            PhotonNetwork.ConnectUsingSettings(verNum);
+            Debug.Log("Starting Connection");
+        }
+
+        public void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinOrCreateRoom(roomName, null, null);
+            Debug.Log("Starting Server!");
+        }
+
+        public void OnJoinedRoom()
+        {
+            isConnected = true;
+            spawnPlayer();
+        }
+
+        public void spawnPlayer()
+        {
+            GameObject pl = PhotonNetwork.Instantiate(playerPref.name, spawnPoint.position, spawnPoint.rotation, 0) as GameObject;
+            GameObject warehouse = PhotonNetwork.Instantiate("Warehouse", spawnPoint.position, spawnPoint.rotation, 0) as GameObject;
+            pl.GetComponent<RigidbodyFPSController>().enabled = true;
+            pl.GetComponent<RigidbodyFPSController>().fpsCam.SetActive(true);
+        }
+    }
